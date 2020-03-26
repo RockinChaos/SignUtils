80b3d74cf872431da33727dbe9aa93e54d88ca6b
RockinChaos <JacobGaming@CraftationGaming.com>
2020-03-25 21:47:02 -0700

    Support for MC 1.15

--- src/me/RockinChaos/signutils/SignUtils.java
+++ src/me/RockinChaos/signutils/SignUtils.java
@@ -1,31 +1,29 @@
 package me.RockinChaos.signutils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.RockinChaos.signutils.handlers.ConfigHandler;
-import me.RockinChaos.signutils.handlers.MemoryHandler;
 import me.RockinChaos.signutils.handlers.ServerHandler;
-import me.RockinChaos.signutils.handlers.UpdateHandler;
 
 public class SignUtils extends JavaPlugin {
-  private static SignUtils instance;
+	private static SignUtils instance;
   
 	@Override
 	public void onEnable() {
-      instance = this;
-      ConfigHandler.loadConfigs();
-      MemoryHandler.setUpdater(new UpdateHandler(getFile()));
-      MemoryHandler.generateData();
-      MemoryHandler.registerEvents();
-      ServerHandler.sendConsoleMessage("&ahas been Enabled.");
+		instance = this;
+		ConfigHandler.generateData(getFile());
+		ConfigHandler.registerEvents();
+		ServerHandler.sendConsoleMessage("&ahas been Enabled.");
 	}
 	
 	@Override
 	public void onDisable() {
+		Bukkit.getScheduler().cancelTasks(this);
 		ServerHandler.sendConsoleMessage("&4has been Disabled.");
 	}
   
-  public static SignUtils getInstance() {
-	  return instance;
-  }
+    public static SignUtils getInstance() {
+	    return instance;
+    }
 }
